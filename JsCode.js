68c0ac7cc09c774afd2dffdaf29d251c353c8ac9
Othenin-Girard Jonathan <OtheninGJ@s2.rpn.ch>
2017-05-11 16:24:36 +0200

    Change verson jQuery 3.2.1

--- JsCode.js
+++ JsCode.js
@@ -1,28 +1,27 @@
-﻿// by Chtiwi Malek on CODICODE.COM
 
 var mousePressed = false;
 var lastX, lastY;
 var ctx;
 
-function InitThis() {
-    ctx = document.getElementById('myCanvas').getContext("2d");
+function Paint() {
+    ctx = document.getElementById('brushBox').getContext("2d");
 
-    $('#myCanvas').mousedown(function (e) {
+    $('#brushBox').mousedown(function (e) {
         mousePressed = true;
         Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
     });
 
-    $('#myCanvas').mousemove(function (e) {
+    $('#brushBox').mousemove(function (e) {
         if (mousePressed) {
             Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
         }
     });
 
-    $('#myCanvas').mouseup(function (e) {
+    $('#brushBox').mouseup(function (e) {
         mousePressed = false;
     });
 
-    $('#myCanvas').mouseleave(function (e) {
+    $('#brushBox').mouseleave(function (e) {
         mousePressed = false;
     });
 }
